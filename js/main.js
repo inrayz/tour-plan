7686512e90a979ccf4070c0d9e169bdf03281013
inrayz <inrayz@bk.ru>
2020-12-03 16:12:18 +0600

    php

--- js/main.js
+++ js/main.js
@@ -37,3 +37,30 @@ menuButton.addEventListener('click', function () {
   console.log('Клик по кнопке')
   document.querySelector(".navbar__nav").classList.toggle('navbar__nav_visible')
 });
+
+// Отправка данных на сервер
+function send(event, php){
+console.log("Отправка запроса");
+event.preventDefault ? event.preventDefault() : event.returnValue = false;
+var req = new XMLHttpRequest();
+req.open('POST', php, true);
+req.onload = function() {
+	if (req.status >= 200 && req.status < 400) {
+	json = JSON.parse(this.response); // Ебанный internet explorer 11
+    	console.log(json);
+        
+    	// ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+    	if (json.result == "success") {
+    		// Если сообщение отправлено
+    		alert("Сообщение отправлено");
+    	} else {
+    		// Если произошла ошибка
+    		alert("Ошибка. Сообщение не отправлено");
+    	}
+    // Если не удалось связаться с php файлом
+    } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+
+// Если не удалось отправить запрос. Стоит блок на хостинге
+req.onerror = function() {alert("Ошибка отправки запроса");};
+req.send(new FormData(event.target));
+}
